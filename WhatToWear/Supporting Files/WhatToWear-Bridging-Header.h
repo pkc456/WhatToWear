fba89c78779318699447a192c6befcf4568e964e
chaudhary.pardeepk <chaudhary.pardeepk@gmail.com>
2016-03-15 23:50:18 +0530

    initial commit without pods

---
+++ WhatToWear/Supporting Files/WhatToWear-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  WhatToWear-Bridging-Header.h
+//  WhatToWear
+//
+//  Created by Pardeep on 15/03/16.
+//  Copyright © 2016 www.programmingcrew.in. All rights reserved.
+//
+
+#ifndef WhatToWear_Bridging_Header_h
+#define WhatToWear_Bridging_Header_h
+
+
+#endif /* WhatToWear_Bridging_Header_h */
