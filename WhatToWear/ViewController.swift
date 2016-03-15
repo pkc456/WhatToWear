231e30bb1af36290b67e38fa9e75e9905b1a4e41
chaudhary.pardeepk <chaudhary.pardeepk@gmail.com>
2016-03-15 22:42:21 +0530

    Initial files
    Added initial files

---
+++ WhatToWear/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatToWear
+//
+//  Created by Pardeep on 15/03/16.
+//  Copyright © 2016 www.programmingcrew.in. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
